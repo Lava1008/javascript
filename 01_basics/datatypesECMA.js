b4f565ad5244d0fab27ce7ab5f6f93adccdacef1
Lavanya Garg <139563625+Lava1008@users.noreply.github.com>
2024-08-11 09:09:26 +0000

    about datatypes

---
+++ 01_basics/datatypesECMA.js
@@ -0,0 +1,31 @@
+"use strict " ; //treat all js code as newer version 
+console.log("lava")
+// alert(26 + 5) we are using node not browser
+
+console.log(3 + 3 + " lava") ;
+// console.log(3 + 3 + " lava") ;console.log(3 + 3 + " lava") ;
+// bad practice because of code readability 
+
+
+let name = "lava"  // string
+let age  = 20  // int 2 to power 53
+// bigint 
+let state = null //means nothing received but defined 
+let boooltype = false ;
+// null is standalone value 
+// undefined => 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof("lava"))
+
+console.log(typeof null ) //object
+console.log(typeof undefined ) //undefined 
+
+
+
+
+
+ 
