6c8ffb081f3f4d5abac1991cf577422338cc270f
Lavanya Garg <139563625+Lava1008@users.noreply.github.com>
2024-08-15 19:38:39 +0000

    Methods of string

---
+++ 01_basics/String.js
@@ -0,0 +1,33 @@
+// concatinate
+const name = "lava" 
+const repoCount = 50 
+
+// console.log(  + + + +) bad practice
+
+// string interpolation 
+console.log(`my name is ${name} and repocount is ${repoCount}`) ; 
+
+const gameName  = new String('lava-garg')
+console.log(gameName);
+console.log(gameName[5]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(name.length);
+console.log(gameName.charAt(5));
+console.log(gameName.indexOf('g'));
+
+
+
+const newString1 = gameName.substring(0,4) 
+// const newString2 = gameName.slice(-5,4) 
+console.log(newString1);
+console.log(newString2);
+
+
+
+// trim and replace 
+// trim remove spaces
+// .replace("which one to replace","by whom") 
+// .includes('lava') ---->true
+
+ 
