8f9a3cbd4a22b2b810cb2ebf29830156c1076fca
Lavanya Garg <139563625+Lava1008@users.noreply.github.com>
2024-08-16 07:59:00 +0000

    Operation on Number

---
+++ 01_basics/numAndMath.js
@@ -0,0 +1,29 @@
+const score = 100 ;
+console.log(score)
+
+const balance  = new Number(100);
+console.log(balance) ; 
+
+console.log(balance.toString())
+console.log(balance.toString().length)
+
+console.log(balance.toFixed(2));
+const otherNum = 23.9996
+const otherNum1 = 123.9996
+// to fixed for decimal values
+
+console.log(otherNum.toFixed(2));
+console.log(otherNum1.toFixed(2));
+
+// precision value
+const otherNum2 = 1123.8966
+
+console.log(otherNum1.toPrecision(3))
+console.log(otherNum1.toPrecision(2))
+
+const hundreds = 1000000000;
+// have commas
+console.log(hundreds.toLocaleString('en-IN'))
+
+// ******Maths**********
+
