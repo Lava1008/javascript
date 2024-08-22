7f3e8fa4c8bf88e70539258342714f297e13ea95
Lavanya Garg <139563625+Lava1008@users.noreply.github.com>
2024-08-22 19:33:39 +0000

    learn about array part 1

---
+++ 01_Arrays/01arrays.js
@@ -0,0 +1,4 @@
+//  difference between slice and splice 
+// slice does not include last part of range and do not change the original array
+// splice includes last part of range and do not change the original array
+// join changes data types
